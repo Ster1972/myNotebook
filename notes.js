be173a88c4dafff8e8917aa3b3c1f568ad28715e
Bob Streeter <streeterb@bell.net>
2024-05-22 11:38:03 -0400

    update

--- notes.js
+++ notes.js
@@ -1,20 +1,115 @@
 
   const notes = [
-    {id: 1, title: "AllDup", category: "Utility", link: "https://alldup.info/", comment: "AllDup is a freeware tool for searching and removing file duplicates on your Windows computer. The fast search algorithm find duplicates of any file type, e.g., text, pictures, music or movies"},
-    {id: 2, title: "Belarc Advisor", category: "Utility", link: "https://www.belarc.com/products/belarc-advisor", comment: "The Belarc Advisor displays a detailed profile of your installed software, hardware and security vulnerabilities in your Web browser. The Belarc Advisor is licensed for personal use only and is not permitted to be used for any commercial or government purposes. Download link: https://www.belarc.com/products/belarc-advisor/download"},
-    {id: 3, title: "ChatGPT", category: "AI Tool", link: "https://chat.openai.com", comment: "ChatGPT is designed to engage in text-based conversations with users, providing responses that are contextually relevant and coherent."},
-    {id: 4, title: "Ctrl + Alt + T ( Launch Terminal in Linux)", category: "Linux Keyboard shortcut", link: "", comment: "Used to launch a terminal window" },
-    {id: 5, title: "Delete garbage files from Windows", category: "Windows help", link: "https://www.youtube.com/watch?v=U4132uo1oXc&t=9s", comment: "Improve your Windows performance, fix Windows issues, and reclaim hard-drive space back when you follow these steps."},
-    {id: 6, title: "Everything (Search tool)", category: "Windows Utility", link: "https://www.voidtools.com/", comment: "Locate files and folders by name instantly"},
-    {id: 7, title: "Gemini (Google)", category: "AI Tool", link: "https://gemini.google.com/app", comment: "Allows you to access Google AI model. Similar to ChatGPT."},
-    {id: 8, title: "LockHunter", category: "Window Utility", link: "https://lockhunter.com/index.htm", comment: "It is a free tool to delete files blocked by something you do not know. LockHunter is useful for fighting against malware, and other programs that are blocking files without a reason. Unlike other similar tools it deletes files into the recycle bin so you may restore them if deleted by mistake"},
-    {id: 9, title: "Meta AI", category: "AI Tool", link: "https://www.meta.ai", comment: "Meta AI is an intelligent assistant that is capable of complex reasoning, following instructions, visualizing ideas, and solving nuanced problems. Competition to ChatGPT."},
-    {id: 10, title: "Node.js Passport Login System Tutorial", category: "YouTube", link: "https://www.youtube.com/watch?v=-RCnNyD0L-s", comment: "Video on user authentication and login."},
-    {id: 11, title: "Norton Bootable Recovery Tool", category: "Recovery Tool", link: "https://support.norton.com/sp/static/external/tools/nbrt.html", comment: "Allows you to create an USB/DVD bootable antivirus checker. This tool is designed to help you when your computer is in dire state due to infection or other issues."},
-    {id: 12, title: "ToffeeShare (file sharing)", category: "Utility", link: "https://toffeeshare.com ", comment: "Share files directly (peer to peer) from any device. Uses webrtc datachannel to perform large file transfer."},
-    {id: 13, title: "Vaildrive", category: "USB Utility", link: "https://www.grc.com/validrive.htm", comment: "Quickly spot-check any USB-connected mass storage drive for read/write errors and fraud."},
-    {id: 14, title: "WiFi password revealer", category: "Windows Utility", link: "https://www.magicaljellybean.com/wifi-password-revealer/", comment: "WiFi password revealer (finder) is a small freeware utility which will show you all your saved WiFi passwords"},
-    {id: 15, title: "Windows Key + D (Access Desktop)", category: "Windows Keyboard shortcut", link: "", comment: "This shortcut gets the user to the desktop without having to adjust any of the open windows"},
-    {id: 16, title: "Windows Key + H (Voice Dictation)", category: "Windows Keyboard shortcut", link: "", comment: "Shortcut key to open the Microsoft Speech Services which gives you access to voice dictation"}
-    
+    {
+      "title": "AllDup",
+      "category": "Utility",
+      "link": "https://alldup.info/",
+      "comment": "AllDup is a freeware tool for searching and removing file duplicates on your Windows computer. The fast search algorithm find duplicates of any file type, e.g., text, pictures, music or movies",
+      "id": "e719df4c-382c-49c4-a9f3-1ad5e39082cc"
+    },
+    {
+      "title": "Belarc Advisor",
+      "category": "Utility",
+      "link": "https://www.belarc.com/products/belarc-advisor",
+      "comment": "The Belarc Advisor displays a detailed profile of your installed software, hardware and security vulnerabilities in your Web browser. The Belarc Advisor is licensed for personal use only and is not permitted to be used for any commercial or government purposes. Download link: https://www.belarc.com/products/belarc-advisor/download",
+      "id": "04325048-1952-4447-b219-95139c1a131e"
+    },
+    {
+      "title": "ChatGPT",
+      "category": "AI Tool",
+      "link": "https://chat.openai.com",
+      "comment": "ChatGPT is designed to engage in text-based conversations with users, providing responses that are contextually relevant and coherent.",
+      "id": "150617b8-fcb7-4594-a5bc-d0212513dee1"
+    },
+    {
+      "title": "Ctrl + Alt + T ( Launch Terminal in Linux)",
+      "category": "Linux Keyboard shortcut",
+      "link": "",
+      "comment": "Used to launch a terminal window",
+      "id": "05b8bbe9-4623-4e5a-8c52-e8ce8b838b5d"
+    },
+    {
+      "title": "Delete garbage files from Windows",
+      "category": "Windows help",
+      "link": " https://www.youtube.com/watch?v=U4132uo1oXc&t=9s",
+      "comment": "Improve your Windows performance, fix Windows issues, and reclaim hard-drive space back when you follow these steps.",
+      "id": "63d22648-ba1d-4ede-9b56-1da28acfb145"
+    },
+    {
+      "title": "Everything (Search tool)",
+      "category": "Windows Utility",
+      "link": "https://www.voidtools.com/",
+      "comment": "Locate files and folders by name instantly",
+      "id": "24c8d52c-cc4d-46d4-b185-09a95bb6bd73"
+    },
+    {
+      "title": "Gemini (Google)",
+      "category": "AI Tool",
+      "link": "https://gemini.google.com/app",
+      "comment": "Allows you to access Google AI model. Similar to ChatGPT.",
+      "id": "e443f304-c50c-4fa5-97d2-c4a1a1a1f1b7"
+    },
+    {
+      "title": "LockHunter",
+      "category": "Window Utility",
+      "link": " https://lockhunter.com/index.htm",
+      "comment": "It is a free tool to delete files blocked by something you do not know. LockHunter is useful for fighting against malware, and other programs that are blocking files without a reason. Unlike other similar tools it deletes files into the recycle bin so you may restore them if deleted by mistake",
+      "id": "9b2e5417-3a86-435d-9b11-188a5336a7e8"
+    },
+    {
+      "title": "Meta AI",
+      "category": "AI Tool",
+      "link": "https://www.meta.ai",
+      "comment": "Meta AI is an intelligent assistant that is capable of complex reasoning, following instructions, visualising ideas, and solving nuanced problems. Competition to ChatGPT.",
+      "id": "d3846195-1d64-486e-99f7-fb4b2c4e03db"
+    },
+    {
+      "title": "Node.js Passport Login System Tutorial",
+      "category": "YouTube",
+      "link": " https://www.youtube.com/watch?v=-RCnNyD0L-s",
+      "comment": "Video on user authentication and login.",
+      "id": "10f5ba13-7118-4130-87ab-ad5d86fb103e"
+    },
+    {
+      "title": "Norton Bootable Recovery Tool",
+      "category": "Recovery Tool",
+      "link": " https://support.norton.com/sp/static/external/tools/nbrt.html",
+      "comment": "Allows you to create an USB/DVD bootable antivirus checker. This tool is designed to help you when your computer is in dire state due to infection or other issues.",
+      "id": "c09b608f-5dc5-48d9-ad0e-18ec10c10363"
+    },
+    {
+      "title": "ToffeeShare (file sharing)",
+      "category": "Utility",
+      "link": "https://toffeeshare.com",
+      "comment": "Share files directly (peer to peer) from any device. Uses webrtc datachannel to perform large file transfer.",
+      "id": "3f7a5ad8-977b-4223-8f25-20997d11c0d3"
+    },
+    {
+      "title": "Vaildrive",
+      "category": "USB Utility",
+      "link": "https://www.grc.com/validrive.htm",
+      "comment": "Quickly spot-check any USB-connected mass storage drive for read/write errors and fraud.",
+      "id": "4bce1026-5f2f-405e-9205-f3fbfdf58641"
+    },
+    {
+      "title": "WiFi password revealer",
+      "category": "Windows Utility",
+      "link": "https://www.magicaljellybean.com/wifi-password-revealer/",
+      "comment": "WiFi password revealer (finder) is a small freeware utility which will show you all your saved WiFi passwords",
+      "id": "6476d735-6985-4511-babc-01f8da39cda3"
+    },
+    {
+      "title": "Windows Key + D (Access Desktop)",
+      "category": " Windows Keyboard shortcut",
+      "link": "",
+      "comment": "This shortcut gets the user to the desktop without having to adjust any of the open windows",
+      "id": "458385b8-a9b6-4130-a6eb-a5edb42d9595"
+    },
+    {
+      "title": "Windows Key + H (Voice Dictation)",
+      "category": "Windows Keyboard shortcut",
+      "link": "",
+      "comment": "Shortcut key to open the Microsoft Speech Services which gives you access to voice dictation",
+      "id": "fff55e70-7d74-4e9f-baae-cfe8cbcdc3c9"
+    }
   ]
