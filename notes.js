3daa0622e70ad156ed6bff8a151bd355bd7a13f3
Bob Streeter <streeterb@bell.net>
2024-09-25 19:15:12 -0400

    update

--- notes.js
+++ notes.js
@@ -140,13 +140,6 @@ const notes = [
     "id": "5ba5b0ef-befc-49ee-bf75-d4fd2c957113"
   },
   {
-    "title": "VS Code keyboard shortcuts",
-    "category": "VS Code",
-    "link": "",
-    "comment": "Ctrl + Z (undo changes)\r\nCtrl + Y (redo changes)\r\nCtrl + Shift + P (Show Command Palette)",
-    "id": "f1ac2f6e-7c37-4754-a9ce-02991bf57713"
-  },
-  {
     "title": "Adafruit Blog",
     "category": "Tech News",
     "link": "https://blog.adafruit.com",
@@ -292,5 +285,285 @@ const notes = [
     "link": "https://www.zdnet.com",
     "comment": "ZDNET is a technology news website that provides the latest updates, reviews and advice on various tech innovations and products which include software, hardware, cyber-security and emerging technologies.",
     "id": "c2cbb15a-fb85-4ac7-9006-cd9f20d56da2"
+  },
+  {
+    "title": "Ctrl + C",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Copy selected text or item",
+    "id": "e68ab506-8e2c-4520-9175-0bd593a3f8ba"
+  },
+  {
+    "title": "Ctrl + V",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Paste copied text or item",
+    "id": "cf931f3c-93d8-4af0-8047-96ad7be823bc"
+  },
+  {
+    "title": "Ctrl + X",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Cut selected text or item",
+    "id": "e7a1cddd-28ce-40b8-b1d2-741992bd9ed6"
+  },
+  {
+    "title": "Ctrl + Z",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Undo last action",
+    "id": "57d7eb73-1c80-4af0-9640-079b005ae2ca"
+  },
+  {
+    "title": "Ctrl + Y",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Redo the last action",
+    "id": "7eca3e0e-3d14-4815-9c09-77ad0ac5edd3"
+  },
+  {
+    "title": "Ctrl + A",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Select all items in a document or window",
+    "id": "e6257d0c-68b7-424f-9ac6-c946e67a99ff"
+  },
+  {
+    "title": "Ctrl + F",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Open the Find dialog to search for text with a document or web page",
+    "id": "c0e278d4-4738-4cef-9b5a-d38c1c49f039"
+  },
+  {
+    "title": "Ctrl + S ",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Save the current document or file",
+    "id": "11f26a6d-d00c-451d-af97-d38f30c7d1e0"
+  },
+  {
+    "title": "Ctrl + P",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Print the current document or page",
+    "id": "e6cc5112-aaa1-4b7f-983d-7d806630ae76"
+  },
+  {
+    "title": "Ctrl + Shift + T",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Reopen the last closed tab in a browser",
+    "id": "c04b9a9f-f912-41a8-8684-248368c1b9df"
+  },
+  {
+    "title": "Alt + Tab",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Switch between open applications",
+    "id": "2e86f5bc-ccaa-45cd-8eb8-91608b2e3797"
+  },
+  {
+    "title": "Alt + F4",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Close the active window or application",
+    "id": "6d49512a-27d9-4a54-a8ee-2f6c7567db24"
+  },
+  {
+    "title": "Windows Key + D",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Minimize all windows and show the decktop",
+    "id": "c804e3d3-2b03-4796-8f0b-29ab4dcf354b"
+  },
+  {
+    "title": "Windows Key + E",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Open File Explorer",
+    "id": "3ece0ec6-1bc8-4929-b74c-59d139879cf7"
+  },
+  {
+    "title": "Windows Key + Tab",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Open Task View to see all open windows",
+    "id": "26cebf41-7dd3-497a-962d-11d27705c46f"
+  },
+  {
+    "title": "Windows Key + S",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Open the Search bar to find apps, file, or web results",
+    "id": "f6b102ff-89d4-495b-b9e4-7ed523eddeb3"
+  },
+  {
+    "title": "Wi;ndows + I",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Open Settings",
+    "id": "20e6c607-9748-4a3d-9de8-728cc89cd4c2"
+  },
+  {
+    "title": "Ctrl + Shift + Esc",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Open Task Manager directly",
+    "id": "06dcd3aa-4a31-42f9-9391-a0b7159a5778"
+  },
+  {
+    "title": "Ctrl + Windows Key + Left/Right Arrow",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Snap windows to the left or right of the screen, or switch between virtual desktops",
+    "id": "60ff9699-972e-4778-92f5-f3c88b2a1fcf"
+  },
+  {
+    "title": "Ctrl + N",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Open a new window or document",
+    "id": "e173a52f-26ba-464d-91f0-334bdd169cfe"
+  },
+  {
+    "title": "Ctrl + R or F5",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Refresh the current page or window",
+    "id": "7ec4f8cd-b8f1-48a2-9442-c2de7aa3a8ac"
+  },
+  {
+    "title": "Windows Key + Up Arrow",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Maximize the current window",
+    "id": "c3329b27-392e-4553-82e3-cd14205b3884"
+  },
+  {
+    "title": "Windows Key + Down Arrow",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Minimize the current window or restore it if it's maximized",
+    "id": "d821d316-509b-4528-acea-b00796a4b695"
+  },
+  {
+    "title": "Windows  Key + P",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Open display settings to project to a second screen or manage multiple monitors",
+    "id": "87a8274f-0732-4e4a-a331-d6322b58fccf"
+  },
+  {
+    "title": "Windows Key + \"+\"",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Open the Magnifier tool for zooming in on the screen",
+    "id": "b8ca9aca-0412-4b90-93fc-ecf12ba2c2e5"
+  },
+  {
+    "title": "F2",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Rename the selected file or folder",
+    "id": "ed72fccc-a7d9-4fb4-a0cb-2d11b07b5294"
+  },
+  {
+    "title": "Ctrl + Shift + Delete",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Open options to clear browsing data in most browsers",
+    "id": "39815daf-e08f-494b-ae35-4602e47b898a"
+  },
+  {
+    "title": "Ctrl + Right/Left Arrow",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Move the cursor to the beginning of the next or previous word in text editing",
+    "id": "602c9190-8266-4ea1-9937-10bcb3af3a05"
+  },
+  {
+    "title": "Ctrl + Backspace",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Delete the previous word in text editing",
+    "id": "29832c1a-5bcd-4259-ad89-359ba9e6a899"
+  },
+  {
+    "title": "Shift + Delete",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Permanently delete selected items (bypasses the Recycle Bin)",
+    "id": "f6c8f937-d089-4fc3-b4b8-7b8474c13967"
+  },
+  {
+    "title": "Alt + Enter",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Open properties for the selected item (file, folder, etc...)",
+    "id": "b69bcc0e-8e16-44cb-9698-cf0e3c939278"
+  },
+  {
+    "title": "Windows Key + V",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Open the clipboard history",
+    "id": "d6a9d6ff-583a-47f3-b38e-9c94de764a46"
+  },
+  {
+    "title": "Windows Key + Ctrl + D",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Create a new virtual desktop",
+    "id": "1ea8640d-32e0-4b2f-986c-eaef57c958d6"
+  },
+  {
+    "title": "Ctrl + T",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Open a new tab",
+    "id": "0e7131e4-43ec-430e-93d8-cc4254387cf2"
+  },
+  {
+    "title": "Ctrl + W",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Close the current tab",
+    "id": "22081c7f-60fe-423d-a662-d48d94acbe2a"
+  },
+  {
+    "title": "Ctrl + Shift + T",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Reopen the last closed tab",
+    "id": "3f122c97-631b-487d-b5ff-da8eddc707e6"
+  },
+  {
+    "title": "Ctrl + T",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Switch to the next open tab",
+    "id": "ba843386-dd4d-40fb-9ac5-5d3ba8ee95e3"
+  },
+  {
+    "title": "Ctrl +Shift + Tab",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Switch to the previous open tab",
+    "id": "ea6c49d8-0fba-4b87-a29c-2a85b13e404b"
+  },
+  {
+    "title": "Ctrl + L",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Highlight the full address bar",
+    "id": "71bd926f-ec17-4c07-885f-628ebb1c2fd3"
+  },
+  {
+    "title": "Ctrl + Shift + N",
+    "category": "Windows Keyboard shortcut",
+    "link": "",
+    "comment": "Open a new private or incognito window (works in most browsers)",
+    "id": "878f40d6-7e25-4d5c-9def-fe55d7636380"
   }
 ]
